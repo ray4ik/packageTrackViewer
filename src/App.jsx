da8791a779502d8ac90e5ee0aa60b8534fc22af6
LenaRay <photoinstockholm@gmail.com>
2020-11-05 12:53:56 +0100

    added parsing of all objects form information

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,19 @@ import "./styles/style.css"
 
 
 function App() {
-console.log(information);
+
+//Instead of <Card data={information[0]} />
+//<Card data={information[1]} />
+
+const Cards = information.map((item) => {
+  return <Card key= {item.id} data={item} />
+}
+);
+
   return (
     <div className="App">
       <h1>Package track Viewer </h1>
-      <Card data={information[0]} />
+      {Cards}
     </div>
   );
 }
